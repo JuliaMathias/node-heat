cb5ae6ce95d56577d55ee0d29e54f3871148fcb4
JuliaMathias <juliamss@gmail.com>
2021-10-19 21:02:27 -0300

    feat: display last 3 messages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import "dotenv/config";
-import express from 'express';
+import express from "express";
+import http from "http";
+import cors from "cors";
 
-import { router } from './routes';
+import { Server } from "socket.io";
+
+import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected to socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
@@ -18,6 +36,6 @@ app.get("/signin/callback", (request, response) => {
   const { code } = request.query;
 
   return response.json(code);
-})
+});
 
-app.listen(4000, () => console.log('🚀 Server is running on port 4000'));
+export { serverHttp, io };
